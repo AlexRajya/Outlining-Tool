e6302604e985f5b04a40bef2137f66ede71c6b28
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2019-11-11 00:20:21 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-  console.log(req.body);
-  fs.writeFileSync("code.txt",JSON.stringify(req.body)) 
+  console.log("saved successfully");
+  fs.writeFileSync("code.txt",JSON.stringify(req.body))
 });
 
 const port = 8000;
