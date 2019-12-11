e6f0d34aa1fc92e89fb366f8dc730846f9bfeb0f
AlexRajya <55755818+AlexRajya@users.noreply.github.com>
2019-12-11 00:31:03 +0000

    Add files via upload
    added websocket

--- server.js
+++ server.js
@@ -1,24 +1,54 @@
 
+//Libraries
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const app = express();
 const fs = require('fs');
+const http = require('http');
+const ws = require('ws');
+const ip = require("ip");
 
-// viewed at http://localhost:8080
+//constants
+const port = process.env.PORT || 8080;
+const app = express();
+const server = http.createServer(app);
+const wss = new ws.Server({ server: server });
+
+//server functions
+function messageHandler(message) {
+    wss.clients.forEach((client) => {
+        if(client.readyState === client.OPEN) {
+            try {
+                client.send(message);
+            } catch (err) {
+              // errors are ignored
+            }
+        }
+    });
+}
+
+function connectionHandler(ws) {
+  console.log("new connection");
+  ws.on('message', messageHandler);
+}
+
+wss.on('connection', connectionHandler);
+
+app.use(express.static(`${__dirname }/public`));
 app.use(express.static(__dirname));
-app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/save', (req) => {
-  console.log('saved successfully');
-  fs.writeFileSync('code.txt', JSON.stringify(req.body));
+app.post('/save', (req, res) => {
+  try{
+    fs.writeFileSync('code.txt', JSON.stringify(req.body));
+    res.sendStatus(200);//OK
+    console.log("saved");
+  } catch (err){
+    res.sendStatus(400);//bad request
+  }
 });
 
-const port = 8080;
-
-app.listen(port, () => {
-  console.log(`Server running on port:${port}`);
+// start the server
+server.listen(port, () => {
+    console.log('Server started:', `http://${ip.address()}:${port}` )
 });
